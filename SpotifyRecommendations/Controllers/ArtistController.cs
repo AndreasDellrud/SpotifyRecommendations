ed5066dd5486c53061cff3eec5918ad75575c192
Andreas Dellrud <andreas.dellrud@gmail.com>
2019-03-07 19:01:50 +0100

    Remove unnecesary usings

--- SpotifyRecommendations/Controllers/ArtistController.cs
+++ SpotifyRecommendations/Controllers/ArtistController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using SpotifyRecommendations.Entities;
 using SpotifyRecommendations.Models;
 using SpotifyRecommendations.Services;
 
